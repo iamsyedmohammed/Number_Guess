8fb118ef1c016e72a715e256e204e5281e3d2d33
SyedMohammed <syedmohdah@gmail.com>
2022-12-23 14:30:47 +0530

    Second Commit

--- numguess.py
+++ numguess.py
@@ -1,17 +1,23 @@
 import random
-inte=random.randint(0,10)
+inte=random.randint(1,10)
 guess=0
+count=0
 for i in range(7):
     num=int(input("Enter a number between 1 & 10:\n"))
     if num==inte:
         guess+=1
+        count+=1
         print(f"You Have Guessed the Number Correctly i.e {inte}")
         break
     elif num>=inte:
         guess+=1
+        count += 1
         print("Your Guess is too High, Please Enter a Lower Number")
     elif num<=inte:
-        print("Your Guess is too Low,Plaese Enter a Higher Number")
+        print("Your Guess is too Low, Please Enter a Higher Number")
         guess+=1
-print(f'You took {guess} attempts')
+        count += 1
+print(f'You took {guess} attempts to guess the number')
+if guess>=7:
+    print("You Have Exceeded the Number of attempts")
 
